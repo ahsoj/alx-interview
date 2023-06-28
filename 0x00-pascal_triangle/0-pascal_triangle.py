9359c1fcd9ace025b701f8a308dbb7c422e4a70d
eyasu <daa282386@gmail.com>
2023-06-28 17:00:47 +0300

    pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+from math import factorial
+
+
+def pascal_triangle(n):
+    _pascal = []
+    if n <= 0:
+        return []
+    for i in range(n):
+        pascal = []
+        for j in range(i + 1):
+            pascal.append(factorial(i) // (factorial(j) * factorial(i - j)))
+        _pascal.append(pascal)
+    return _pascal
