5f143ad9bd0e7f16b6f400a3a88ec7ca843b0f23
eyasu <daa282386@gmail.com>
2023-08-02 10:50:27 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """N queens"""
 
-import sys
-
 
 def nQueens(n: int, i, a, b, c):
     """N queens puzzle"""
@@ -14,16 +12,19 @@ def nQueens(n: int, i, a, b, c):
         yield a
 
 
-args = sys.argv
-if len(args) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-elif not args[1].isdigit():
-    print("N must be a number")
-    sys.exit(1)
-elif int(args[1]) < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-else:
-    for solution in nQueens(int(args[1]), 0, [], [], []):
-        print([[idx, sol] for idx, sol in enumerate(solution)])
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv
+    if len(args) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    elif not args[1].isdigit():
+        print("N must be a number")
+        exit(1)
+    elif int(args[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
+    else:
+        for solution in nQueens(int(args[1]), 0, [], [], []):
+            print([[idx, sol] for idx, sol in enumerate(solution)])
