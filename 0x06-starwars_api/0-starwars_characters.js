b75b8d50c582b85593bf61e1382059546e9aa52e
eyasu <daa282386@gmail.com>
2023-08-10 15:57:28 +0300

    starwars-api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
+/**
+ * return the character names
+ * of specified films by id
+ * @params integer
+ */
+
 const request = require('request');
 
 const endpoint = 'https://swapi-api.alx-tools.com/api';
 const filmId = process.argv[2];
 
-request(`${endpoint}/films/${filmId}/`, async (error, _res, body) => {
+request(`/${endpoint}/films/${filmId}/`, async (error, _res, body) => {
   if (error) return console.log(error);
 
   const characters = await JSON.parse(body).characters;
